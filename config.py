3dca7da7bc72c117b4102871bb19c337237d1865
javialvarezdrive <153924249+javialvarezdrive@users.noreply.github.com>
2025-03-06 09:13:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Configura tu conexión a Supabase
-SUPABASE_URL = "https://your-supabase-url.supabase.co"
-SUPABASE_KEY = "your-supabase-key"
+SUPABASE_URL = "https://pqftbzxsiuoyzyopfqrq.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxZnRienhzaXVveXp5b3BmcXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMDMwMTYsImV4cCI6MjA1Njc3OTAxNn0.7mCVsFsDRGYgBfVgLdjHgHycRvUPNdwSgrwqehIFsoY"
